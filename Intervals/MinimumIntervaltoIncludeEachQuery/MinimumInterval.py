5bbb78a818dd7c694d57dbb24541f0a63035dc31
al3913 <al3913@g.rit.edu>
2026-02-08 19:07:03 -0500

    feb 8 optimal solution for minimumInterval

--- Intervals/MinimumIntervaltoIncludeEachQuery/MinimumInterval.py
+++ Intervals/MinimumIntervaltoIncludeEachQuery/MinimumInterval.py
@@ -17,3 +17,31 @@ class Solution:
             result.append(mini)
         
         return result
+
+# Optimal Solution
+import heapq
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        intervals.sort()
+        bookkeeping = {}
+        lastInterval = []
+        n = len(intervals)
+        start = 0
+        for query in sorted(queries):
+            mini = -1
+            while start < n:
+                if intervals[start][0] <= query and query <= intervals[start][1]:
+                    heapq.heappush(lastInterval, (intervals[start][1] - intervals[start][0] + 1, intervals[start][1]))
+                elif intervals[start][0] > query:
+                    break
+                start+=1
+
+            while lastInterval:
+                if query <= lastInterval[0][1]:
+                    mini = lastInterval[0][0]
+                    break
+                else:
+                    heapq.heappop(lastInterval)
+
+            bookkeeping[query] = mini
+        return [bookkeeping[query] for query in queries]
