29a8b8401d3dd2f7b5111698c2ba1f07c4c9446e
al3913 <al3913@g.rit.edu>
2026-02-27 15:24:27 -0500

    feb 27 added original solution for HandOfStraights

--- Greedy/HandOfStraights/HandOfStraights.py
+++ Greedy/HandOfStraights/HandOfStraights.py
@@ -1,3 +1,28 @@
+# Initial Solution
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        n = len(hand)
+        if n % groupSize != 0: return False
+        dupes = defaultdict(int)
+
+        for num in hand:
+            dupes[num] +=1
+
+        groups = n // groupSize
+        while groups != 0:
+            start = min(dupes.keys())
+            for i in range(start, start + groupSize):
+                if i in dupes and dupes[i] > 0:
+                    dupes[i] -=1
+                    if dupes[i] == 0: del dupes[i]
+                else:
+                    return False
+            groups-=1
+        
+        return True
+
+
+# Optimized Sorting Solution
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         n = len(hand)
