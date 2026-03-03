b1d94eb93a072a28f00685d68f847d421f13f8a4
luxoual <linfamilyandy@gmail.com>
2026-03-02 23:53:26 -0500

    mar 2 partitionLabel initial solution

--- Greedy/PartitionLabels/PartitionLabels.py
+++ Greedy/PartitionLabels/PartitionLabels.py
@@ -1,3 +1,42 @@
+# Initial solution
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        # How can we tell we have no more of the letter
+        # in the future? A counter
+
+        counts = Counter(s)
+
+        # We need someway to know that all the letters
+        # in the current substring, are at 0
+
+        # We could use a set which contains the letters
+        # that are present in the current substring
+        
+        curr = set()
+
+        # We need some way to calculate the size, so we 
+        # can keep track of a start and calculate diff
+
+        n = len(s)
+        start = 0
+        result = []
+
+        for i in range(n):
+            if s[i] not in curr:
+                curr.add(s[i])
+            counts[s[i]]-=1
+            if not counts[s[i]]: # Became 0
+                curr.remove(s[i])
+            if not curr:
+                # All letters in our substring have no
+                # more occurences in the rest
+                result.append(i - start + 1)
+                start = i + 1
+        
+        return result
+                
+
+
 # Optimized Two Pointer Greedy
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
