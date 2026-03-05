7414b434a08dd94f4ba0c86768781159fa1304ed
luxoual <linfamilyandy@gmail.com>
2026-03-05 14:07:03 -0500

    mar 5 dfs solution for networkdelaytime

--- AdvancedGraphs/NetworkDelayTime/NetworkDelayTime.py
+++ AdvancedGraphs/NetworkDelayTime/NetworkDelayTime.py
@@ -1,3 +1,4 @@
+# Dijkstra's Algorithm
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         # Dictionary of Dictionarys
@@ -19,3 +20,33 @@ class Solution:
                     heapq.heappush(queue, (costs[curr[1]][nei] + curr[0], nei))
             result = curr[0]
         return result if len(visited) == n else -1
+    
+# DFS solution
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # DFS
+        adj = defaultdict(dict)
+        shortest = [float('inf')] * n
+        for time in times:
+            adj[time[0]][time[1]] = time[2]
+        
+        # i represents what node we are on, and prev
+        # represents how much we have travelled so far
+        # to get here
+        def dfs(i, prev):
+            # Basecase
+            # We are going for the shortest path, so
+            # if we get anything that is greater than
+            # our currently stored shortest path, then
+            # we don't even continue, so we also don't
+            # need to worry about loops
+            if prev >= shortest[i-1]: return
+
+            shortest[i-1] = prev
+            # Else we traverse
+            for nei in adj[i]:
+                dfs(nei, prev + adj[i][nei])
+        
+        dfs(k, 0)
+        result = max(shortest)
+        return result if result != float('inf') else -1
