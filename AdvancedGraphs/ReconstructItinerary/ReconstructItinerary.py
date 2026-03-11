8b1d55ad96ee6ceb4886f571d11167259f724ba4
luxoual <linfamilyandy@gmail.com>
2026-03-10 22:07:02 -0400

    mar 10 dfs Hierholzer Eulerian path solution

--- AdvancedGraphs/ReconstructItinerary/ReconstructItinerary.py
+++ AdvancedGraphs/ReconstructItinerary/ReconstructItinerary.py
@@ -31,3 +31,40 @@ class Solution:
 
         dfs("JFK", len(tickets))
         return result
+
+# DFS Eulerian Path + Hierholzer's Algorithm
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        # Hierholzer's Algorithm for Eulerian Path
+        # This question is a Eulerian Path question
+        # because its asking for you to return the
+        # path that uses up all the tickets/routes
+        # It says that theres at least one Eulerian
+        # Path, so that means we can use this algo.
+
+        # They ask for smallest lexical order path,
+        # which means if we have multiple paths,
+        # we want specifically the one that
+        # prioritizes the smallest lexical order 
+        # choices
+
+        adj = defaultdict(list)
+        tickets.sort(reverse=True)
+        for ticket in tickets:
+            # We have the smallest lexical at the end
+            adj[ticket[0]].append(ticket[1])
+        
+        result = []
+
+        # We know if a node is the current ending
+        # if we run out of neighbors
+        def dfs(airport):
+            while adj[airport]:
+                # While we have neighbors traverse
+                dfs(adj[airport].pop()) # Smallest lexical choice
+            # Only add when we know that we have reached an ending
+            result.append(airport)
+        
+        dfs("JFK")
+        return result[::-1]
+
