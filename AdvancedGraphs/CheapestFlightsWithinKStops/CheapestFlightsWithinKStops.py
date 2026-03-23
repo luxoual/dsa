a2d23accb92fe76539bd785cd34b5dc830ac3ce0
luxoual <linfamilyandy@gmail.com>
2026-03-23 19:37:26 -0400

    dijkstras solution mar 23

--- AdvancedGraphs/CheapestFlightsWithinKStops/CheapestFlightsWithinKStops.py
+++ AdvancedGraphs/CheapestFlightsWithinKStops/CheapestFlightsWithinKStops.py
@@ -34,3 +34,34 @@ class Solution:
             return result
         cheapest = dfs(0, 0, src)
         return -1 if cheapest == float('inf') else cheapest
+
+# Dijkstra's Algorithm (without visited, keeping track of stops because we have to address all the ways to get to a node)
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # We are looking for the shortest path
+        # to dst from src, 
+        adj = defaultdict(list)
+        for flight in flights:
+            # Price and dst
+            adj[flight[0]].append((flight[2], flight[1]))
+
+        # Dijkstras
+        heap = []
+        # Distances to reach [node] in [flights] dist[node][flights] = dist
+        distances = [[float('inf')] * (k+2) for _ in range(n)]
+        distances[src][0] = 0
+        # Cost, Dest, Stops
+        heapq.heappush(heap, (0, src, 0))
+        while heap:
+            cost, dest, stops = heapq.heappop(heap)
+            if dest == dst: return cost
+            # If we haven't reached
+            if stops > k: continue
+            for nei in adj[dest]:
+                newDst = cost + nei[0]
+                # Prune any distances that are worse
+                # than what we already have
+                if distances[nei[1]][stops+1] > newDst:
+                    distances[nei[1]][stops+1] = newDst
+                    heapq.heappush(heap, (newDst, nei[1], stops + 1))
+        return -1
