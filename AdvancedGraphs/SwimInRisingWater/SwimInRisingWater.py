c3bda9bd578ac6e95a7344477c2581023b6e19a5
luxoual <linfamilyandy@gmail.com>
2026-04-04 17:16:18 -0400

    apr 4 dijkstras algo

--- AdvancedGraphs/SwimInRisingWater/SwimInRisingWater.py
+++ AdvancedGraphs/SwimInRisingWater/SwimInRisingWater.py
@@ -1,3 +1,4 @@
+# DFS Backtracking
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         # Backtracking DFS, at each cell we can try all 4 directions
@@ -27,5 +28,37 @@ class Solution:
             return lowest
 
         return dfs(0, 0, grid[0][0])
+
+# Dijkstra's Algorithm
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        # Attempting Dijkstras attempt
+        # visited set with (x,y) coordinates
+        # heap with (water, x, y)
+        # whenever we pop from the heap, its the least
+        # amount of water to reach this cell
+        # so we add it to visited
+        # We traverse through the directions, using
+        # max(water, grid[nx][ny]), if we find any
+        # visited cells, then we just skip this one,
+        # the first time we pop from the heap with
+        # the bottom right cell, we just return
+        visited = set()
+        n = len(grid)
+        heap = [(grid[0][0], 0, 0)]
+        directions = [(0,1), (-1,0), (1,0), (0,-1)]
+        while heap:
+            water, x, y = heapq.heappop(heap)
+            if x == n-1 and y == n-1: return water
+            if (x,y) in visited: continue
+            visited.add((x,y))
+
+            for dx, dy in directions:
+                nx, ny = dx + x, dy + y
+                if 0 <= nx < n and 0 <= ny < n and (nx,ny) not in visited:
+                    heapq.heappush(heap, (max(water, grid[nx][ny]), nx, ny))
+
+
+
 
 
