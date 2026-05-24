d253fee30f2d8f82cbc95d8dbf21337d11b42348
luxoual <linfamilyandy@gmail.com>
2026-05-24 00:22:28 -0400

    quick review of editdistance

--- 2D Dynamic Programming/EditDistance/EditDistance.py
+++ 2D Dynamic Programming/EditDistance/EditDistance.py
@@ -48,3 +48,40 @@ class Solution:
             return result
 
         return dfs(0, 0)
+
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        # Backtracking approach first
+        # at each step, i have the option of doing
+        # one of the 3 operations
+        # if i insert -> i keep my index in word1 but move word2
+        # if i delete -> i move my index in word1 but keep word2
+        # if i replace -> i move my index in word1 and word2
+        # if they are the same -> same thing as replace
+        # When can i end? When I reach the end of a word
+        # the number of operations is however much it took to
+        # get there + the difference in however far I got in
+        # the other word (same thing as inserting each time)
+        n = len(word1)
+        m = len(word2)
+        cache = [[-1] * m for _ in range(n)]
+
+        def dfs(i, j):
+            if i == n:  # if we reach the end of word1 first
+                return m - j
+            if j == m:  # if we reach the end of word2 first
+                return n - i
+            if cache[i][j] != -1:
+                return cache[i][j]
+
+            # if we arent at the end
+            if word1[i] != word2[j]:
+                # try all our options!
+                cache[i][j] = min(dfs(i, j + 1), dfs(i + 1, j))
+                cache[i][j] = min(cache[i][j], dfs(i + 1, j + 1)) + 1
+            else:
+                cache[i][j] = dfs(i + 1, j + 1)
+            return cache[i][j]
+
+        return dfs(0, 0)
