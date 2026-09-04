2faff8f81c3b3923f7645204374dc065d93de4f1
luxoual <linfamilyandy@gmail.com>
2026-09-04 19:54:26 -0400

    plusone quick review

--- Math/PlusOne/PlusOne.py
+++ Math/PlusOne/PlusOne.py
@@ -13,3 +13,16 @@ class Solution:
         if result[0] != 0:
             return result
         return result[1::]
+
+
+# Second Run at it
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        results = digits.copy()
+        for i in range(len(digits) - 1, -1, -1):
+            result = carry + digits[i]
+            results[i] = result % 10
+            if result < 10:
+                return results
+        return results if not carry else [1] + results
