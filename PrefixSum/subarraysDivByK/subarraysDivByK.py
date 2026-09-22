e5676a64d8143363b99a53c90904c70dbfa0af1f
luxoual <linfamilyandy@gmail.com>
2026-09-22 15:10:16 -0400

    subarrays divisible by k solution

---
+++ PrefixSum/subarraysDivByK/subarraysDivByK.py
@@ -0,0 +1,37 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        # Prefix sums?
+        # We were looking for (prefix[j] - prefix[i]) % k == 0
+        # Now when is the difference between 2 numbers
+        # divisible by k?
+        # When 2 numbers have the same remainder after doing % k
+        # then that means when you subtract those 2 numbers,
+        # you'll get a value that is divisible by k
+
+        # k = 5 a = 9 b = 4 c = 14
+        # 9 // 5 = 1 r-4
+        # 4 //  5 = 0 r-4
+        # 14 // 5 = 2 r-4
+
+        # So we are just looking for prefixs that have matching
+        # remainders, everytime we find a matching remainder, we
+        # can make total times we saw that remainder - 1 new pairs
+
+        total = 0
+        res = 0
+        remainders = defaultdict(int)
+        remainders[0] = 1  # Because 0 as a remainder is auto-pair
+
+        for n in nums:
+            total += n  # Current prefix sum
+            rem = total % k  # To grab remainder
+
+            # we can make the same amount of pairs as the number
+            # of times we saw this remainder before, if its the
+            # first time then we don't add anything
+            res += remainders[rem]
+
+            # Increment hte number of times we saw it
+            remainders[rem] += 1
+
+        return res
